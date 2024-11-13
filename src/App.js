1a60472aeb1ea7c7ef135627e24a1d841de99e11
gurbur <jihwan7577@gmail.com>
2024-11-13 12:19:04 +0900

    Feat: Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyNavbar from './components/MyNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <MyNavbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
